fcb22e3e2e9b7ec79e4634aa3dc5c643761e40ba
E4MMahendra <mahendra.gauda@exchange4media.com>
2025-01-04 22:34:04 +0530

    build error sloved

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,28 @@
-"use strict";
+const gulp = require("gulp");
+const del = require("del");
+const sass = require("gulp-sass")(require("sass"));
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
 
-var gulp = require("gulp");
-var sass = require("gulp-sass")(require("sass"));
-var uglify = require("gulp-uglify");
-var rename = require("gulp-rename");
+// Example tasks
+gulp.task("clean", function () {
+  return del(["dist"]);
+});
 
-// compile scss to css
-gulp.task("sass", function () {
+gulp.task("styles", function () {
   return gulp
-    .src("./sass/styles.scss")
+    .src("src/scss/**/*.scss")
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-    .pipe(rename({ basename: "styles.min" }))
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest("dist/css"));
 });
 
-// watch changes in scss files and run sass task
-gulp.task("sass:watch", function () {
-  gulp.watch("./sass/**/*.scss", ["sass"]);
-});
-
-// minify js
-gulp.task("minify-js", function () {
+gulp.task("scripts", function () {
   return gulp
-    .src("./js/scripts.js")
+    .src("src/js/**/*.js")
+    .pipe(concat("app.js"))
     .pipe(uglify())
-    .pipe(rename({ basename: "scripts.min" }))
-    .pipe(gulp.dest("./js"));
-});
-gulp.task("build", function () {
-  // Add tasks here, e.g., compiling, bundling, etc.
-  console.log("Build task executed");
+    .pipe(gulp.dest("dist/js"));
 });
 
-// default task
-gulp.task("default", gulp.series("sass", "minify-js"));
+// Define the build task
+gulp.task("build", gulp.series("clean", "styles", "scripts"));
